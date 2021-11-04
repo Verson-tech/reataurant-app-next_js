30834eabe51ce814c117dc6cfbeae61e40535fee
vira.murray <vira.avramenko@gmail.com>
2021-11-04 15:22:10 +0300

    dynamic linking localhost:3000/restaurants/67 renders "Dynamic Retaurant Page 67"

--- pages/restaurants/[restaurant].js
+++ pages/restaurants/[restaurant].js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 // import styles from '../styles/Home.module.css'
 
 export default function Restaurant() {
+  const router = useRouter()
   return (
-   <h1>Dynamic Restaurant Page</h1>
+   <h1>Dynamic Restaurant Page {router.query.restaurant}</h1>
   )
 }
