1b2cf325a23bec8af047dd3958797d4720b80b7b
vira.murray <vira.avramenko@gmail.com>
2021-11-04 15:39:33 +0300

    set up dynamic links to specific restaurants  Video 28.11

--- pages/restaurants/index.js
+++ pages/restaurants/index.js
@@ -1,8 +1,28 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import styles from '../styles/Home.module.css'
+import Link from "next/Link";
 
 export default function Home() {
+  const restaurants = [
+    { name: "Woodshill" },
+    { name: "Fiorellas" },
+    { name: "Karma" },
+  ];
   return (
-   <h1>Restaurant Top Page</h1>
-  )
+    <div>
+      <h1>Restaurant Top Page</h1>
+      {restaurants.map((item) => {
+        return (
+          <div>
+            <Link
+              as={"/restaurants/" + item.name}
+              href="restaurants/[restaurant]"
+            >
+              <a>{item.name}</a>
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
