74e6756294a8789b8923fb3261f65ad732181ec0
vira.murray <vira.avramenko@gmail.com>
2021-11-04 14:58:11 +0300

    deleted:    pages/restaurants.js added pages/restaurant/ hello.js index.js Video 28.10 @ 5:38

--- pages/restaurant/hello.js
+++ pages/restaurant/hello.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-   <h1>Restaurant Page</h1>
+   <h1>Hello Page</h1>
   )
 }
